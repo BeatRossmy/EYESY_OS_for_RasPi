1f682502d5f652f4cf7fafe0836fb7dbdb798318
BeatRossmy <39985617+BeatRossmy@users.noreply.github.com>
2021-04-16 07:12:35 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ touch /tmp/video.log
 
 #amixer cset numid=11 on
 
-systemctl start eyesy-python.service
-systemctl start eyesy-web.service
-systemctl start eyesy-web-socket.service
-systemctl start eyesy-pd.service
+sudo systemctl start eyesy-python.service
+sudo systemctl start eyesy-web.service
+sudo systemctl start eyesy-web-socket.service
+sudo systemctl start eyesy-pd.service
